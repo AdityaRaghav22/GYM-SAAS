4ea2fd283bf61aaf2c6ec0250053ac426175985b
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-02 16:15:50 +0000

    Fixed

--- gym_saas/app/__init__.py
+++ gym_saas/app/__init__.py
@@ -73,10 +73,6 @@ def create_app():
     @jwt.unauthorized_loader
     def unauthorized_callback(reason):
         return redirect(url_for("api_v1.gym_auth.login_page"))
-
-    @jwt.expired_token_loader
-    def expired_callback(jwt_header, jwt_payload):
-        return redirect(url_for("api_v1.gym_auth.login_page"))
         
     @jwt.invalid_token_loader
     def invalid_token_callback(reason):
