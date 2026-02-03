10d89cf44f191df03e5063a45055e46ecf72bed1
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-03 11:02:25 +0000

    Login Update

--- gym_saas/app/__init__.py
+++ gym_saas/app/__init__.py
@@ -44,7 +44,6 @@ def create_app():
     def expired_callback(jwt_header, jwt_payload):
         return {"msg": "access token expired"}, 401
 
-
     @jwt.invalid_token_loader
     def invalid_token_callback(reason):
         return {"msg": "invalid token"}, 401
