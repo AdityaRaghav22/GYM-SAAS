3e5b452b29c9daf1c5226e86d8f4ee35721d2593
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-03 14:53:55 +0000

    changes

--- gym_saas/app/routes/dashboard.py
+++ gym_saas/app/routes/dashboard.py
@@ -1,13 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask_jwt_extended import verify_jwt_in_request
-from flask_jwt_extended.exceptions import JWTExtendedException
+from flask import Blueprint, render_template
+from flask_jwt_extended import jwt_required
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
-@dashboard_bp.route("/")
+@dashboard_bp.route("/", methods=["GET"])
+@jwt_required()
 def home():
-    try:
-        verify_jwt_in_request()  # STRICT validation
-        return redirect(url_for("api_v1.dashboard.home"))
-    except JWTExtendedException:
-        return render_template("home.html")
+    return render_template("dashboard/home.html")
