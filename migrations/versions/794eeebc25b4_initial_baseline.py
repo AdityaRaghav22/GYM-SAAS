96758e9fdab392ef990280cc277ac26b85c1a2c4
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-18 06:07:02 +0000

    Fixing migrations

---
+++ migrations/versions/794eeebc25b4_initial_baseline.py
@@ -0,0 +1,24 @@
+"""initial baseline
+
+Revision ID: 794eeebc25b4
+Revises: 
+Create Date: 2026-02-18 06:05:35.324455
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '794eeebc25b4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
