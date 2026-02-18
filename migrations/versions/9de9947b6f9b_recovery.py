f61a544bceb3816d6ae830d06a4d80df5d52906b
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-18 13:34:05 +0000

    Fixing migeration issue

---
+++ migrations/versions/9de9947b6f9b_recovery.py
@@ -0,0 +1,23 @@
+"""recovery migration
+
+Revision ID: NEW_ID
+Revises:
+Create Date: 2026-02-18
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers
+revision = "9de9947b6f9b"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
